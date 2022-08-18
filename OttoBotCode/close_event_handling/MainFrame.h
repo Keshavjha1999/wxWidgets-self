fc5f139d0effb4809d05a5386c28e17e6d62c314
Keshav <keshavkjha1999@gmail.com>
2022-08-18 17:38:18 +0530

    Initial commit

---
+++ OttoBotCode/close_event_handling/MainFrame.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <wx/wx.h>
+
+
+class MainFrame : public wxFrame
+{
+public:
+	MainFrame(const wxString& title);
+
+private:
+	void OnAnyButtonClicked(wxCommandEvent& evt);
+	void OnButton1Clicked(wxCommandEvent& evt);
+	void OnButton2Clicked(wxCommandEvent& evt);
+	void OnClose(wxCloseEvent& evt);
+};
+
---
+++ console_application.cpp
@@ -0,0 +1,5 @@
+#include "./wx/string.h"
+
+int main(int argc, char **argv){
+    wxPuts(wxT("A wxWidgets console application"));
+}
